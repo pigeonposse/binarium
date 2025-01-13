bf92b2cb3018eb55541feb722789f977a5d0cf50
angelespejo <angelespejo13@gmail.com>
2025-01-14 00:45:00 +0100

    :memo: docs(env): add pwa

--- .dovenv/main.js
+++ .dovenv/main.js
@@ -99,6 +99,29 @@ export default defineConfig(
 				intro     : false,
 				reference : false,
 			},
+			pwa : { manifest : {  icons : [
+				{
+					src   : 'pwa-64x64.png',
+					sizes : '64x64',
+					type  : 'image/png',
+				},
+				{
+					src   : 'pwa-192x192.png',
+					sizes : '192x192',
+					type  : 'image/png',
+				},
+				{
+					src   : 'pwa-512x512.png',
+					sizes : '512x512',
+					type  : 'image/png',
+				},
+				{
+					src     : 'maskable-icon-512x512.png',
+					sizes   : '512x512',
+					type    : 'image/png',
+					purpose : 'maskable',
+				},
+			] } },
 		},
 		convert : { api : {
 			type : 'custom',
